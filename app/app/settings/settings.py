3f8d4b8d49bbfb6e7c6b06983c0a6c712df93015
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-08-25 22:20:00 +0330

    [IMP] logger

--- app/app/settings/settings.py
+++ app/app/settings/settings.py
@@ -14,3 +14,44 @@ INSTALLED_APPS += APPS
 INSTALLED_APPS += FRAMEWORKS
 
 ASGI_APPLICATION = 'app.asgi.application'
+
+
+
+
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers":False,
+
+    "formatters": {
+        "verbose": {
+            "format": "{levelname} {asctime} {module} {process:d} {thread:d} {message}",
+            "style": "{",
+        },
+        "simple": {
+            "format": "{levelname} : {message}",
+            "style": "{"
+        }
+    },
+    
+    "handlers":{
+        "debug_logs":{
+            "level":"DEBUG",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "logs/DEBUG.log",
+            "formatter": "simple"
+        },
+        "warnings_log":{
+            "level": "WARNING",
+            "class": "logging.FileHandler",
+            "filename": BASE_DIR / "logs/WARNING.log",
+            "formatter": "verbose"
+        },
+    },
+    "loggers":{
+        "":{
+            "level": 'DEBUG',
+            "handlers":["warnings_log",'debug_logs'],
+        },
+
+    }
+}
