37ef6e11aa0c05906664ace85b1de5ed2056e661
Amin Masoudi <aminmasoudi2003@gmail.com>
2023-08-29 13:02:56 +0330

    UPD=> core.models.User

--- app/core/models.py
+++ app/core/models.py
@@ -3,23 +3,9 @@ from django.contrib.auth.base_user import BaseUserManager
 from django.contrib.auth.models import AbstractUser, PermissionsMixin
 from django.utils.translation import gettext_lazy as _
 # Create your models here.
-class UserManager(BaseUserManager):
-    def create_user(self, email, password, **extra):
-        user = self.model(email=self.normalize_email(email), **extra) 
-        user.set_password(password)
-        user.save()
-        return user
-    
-    def create_superuser(self, email, password,**extra):
-        user = self.create_user(email=email, password=password, **extra)
-        user.is_superuser = True
-        user.is_staff = True
-        user.save()
-        return user
     
 class User(AbstractUser, PermissionsMixin):
     ...
-    chats = models.ManyToManyField("core.Chat", verbose_name=_("Users Chat"), related_name="members")
 
 
 class Chat(models.Model):
